da6594de9b99d6ea4f60a04adbc3de942cdcd2ed
Eliza Fury <eliza.fury@onemodel.co>
2022-05-31 09:06:19 +1000

    Initial commit

---
+++ WebApp/WeatherForecast.cs
@@ -0,0 +1,17 @@
+using System;
+
+//Namespace
+namespace WebApp
+{
+    //Main Class
+    public class WeatherForecast
+    {
+        static void Main(string[] args)
+        {
+            string name = "Eliza Fury";
+            //Start here
+            Console.WriteLine(name);
+
+        }
+    }
+}
